c56ce26e14a18020b9355f59be3cc52f772aebea
Spencer Rivette <gaiusjuliuscesar@gmail.com>
2023-01-06 17:14:09 -0800

    simple test

--- src/test/java/org/example/mymath/TestUselessMath.java
+++ src/test/java/org/example/mymath/TestUselessMath.java
@@ -1,9 +1,17 @@
 package org.example.mymath;
 
-public class TestUselessMath {
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+public class TestUselessMath {
     @Test
     public void testSlowInverseSquareRoot() {
+        double startingValue = 4d;
+        double computedValue = UselessMath.slowInverseSqRoot(startingValue);
 
+        assertThat(computedValue, is(greaterThan(1d)));
     }
 }
